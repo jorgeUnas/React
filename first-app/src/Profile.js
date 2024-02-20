7dc6c6e1b5c58ecc5b5c5b9b9841a410d85ab978
EC2 Default User <ec2-user@ip-172-31-25-107.eu-north-1.compute.internal>
2024-02-20 10:31:29 +0000

    including a function in a component to display a nav bar

--- first-app/src/Profile.js
+++ first-app/src/Profile.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import NavBar from './NavBar';
 
 function Picture() {
-  return <img className="octupus" src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg" width="500px" heigth="500px"/>;
+  return <img className="monkey" src="https://content.codecademy.com/courses/React/react_photo-monkeyselfie.jpg" width="500px" heigth="500px"/>;
 }
 
 function Profile() {
-  return (
-    <>
-      <Picture />
-      <h1>Name: Octavia</h1>
-      <h2>Species: Octopus</h2>
-       <h2>Class: Cephalopoda</h2>
-    </>
-  )
+    return (
+    <div>
+      <NavBar />
+      <h1>All About Me!</h1>
+      <p>I like movies and blah blah blah blah blah</p>
+      <div id='selfie'>
+        <Picture />
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
