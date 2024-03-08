c1732ea08673dd5f02a79af3ab3770885cdb0531
EC2 Default User <ec2-user@ip-172-31-25-107.eu-north-1.compute.internal>
2024-03-08 10:57:31 +0000

    Including other assertion in the test

--- grocerylist/src/language_spoken.test.js
+++ grocerylist/src/language_spoken.test.js
@@ -10,12 +10,17 @@ it("converts array of country data objects to array of countries", ()=>{
       {name: "Belize", capital: "Belmopan"},
       {name: "Bolivia", capital: "Sucre"}
       ]
-    const expectedValue = ["Argentina","Belize","Bolivi"]
+    const expectedValue = ["Argentina","Belize","Bolivia"]
     
     //act
     const actualValue = countryExtractor(inputObject)
     
     //assert
     expect(actualValue).toEqual(expectedValue);
+    //expect(actualValue[0] === 'Argentina').toBeTruthy();
+    expect(actualValue[0]).toBe('Argentina');
+    expect(actualValue).toContain('Belize');
+    expect(actualValue[2] === 'Bolivia').toBeTruthy();
+    expect(actualValue[3]).not.toBeDefined();
     
 })
